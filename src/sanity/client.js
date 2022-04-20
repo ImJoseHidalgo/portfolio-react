3c6f59f3accfee72995d369202b97e94dba11ef5
imjosehidalgo <josehidalgo990@gmail.com>
2022-04-19 21:41:51 -0400

    chore: add env and removed unused files

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -1,6 +1,10 @@
 import sanityClient from "@sanity/client";
 
+const { SANITY_ID, SANITY_DATASET } = process.env;
+
 export default sanityClient({
-  projectId: 'np735t1f',
-  dataset: 'production'
+  // projectId: 'np735t1f',
+  // dataset: 'production'
+  projectId: SANITY_ID,
+  dataset: SANITY_DATASET,
 })
